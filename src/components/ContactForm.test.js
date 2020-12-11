e8db4ad93e3bb0233362eeb78c93e91dc8dc5c72
Bradley Dion <bradleydion@MacBook-Pro.local>
2020-12-10 20:28:04 -0500

    finished making the test pass, but the warning still shows

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,22 +6,25 @@ import ContactForm from "./ContactForm"
 test("renders ContactForm component without errors", () =>{
     render(<ContactForm/>)
 })
-test("user can fill out form", ()=>{
+test("user can fill out form", async ()=>{
     render(<ContactForm/>)
     const firstInput = screen.getByLabelText(/first name/i)
     const lastInput = screen.getByLabelText(/last name/i)
     const emailInput = screen.getByLabelText(/email/i)
     const messageInput = screen.getByLabelText(/message/i)
-    const button = screen.getByRole("button",{name:/submit/i})
+    
 
-    act(() => { 
-        userEvent.type(firstInput, "Brad")
-        userEvent.type(lastInput, "Dion")
-        userEvent.type(emailInput, "boy@blueberry.com")
-        userEvent.type(messageInput, "this test is so weird")
-        userEvent.click(button)
-      });
+    
+        userEvent.type(firstInput, "Brad");
+        userEvent.type(lastInput, "Dion");
+        userEvent.type(emailInput, "boy@blueberry.com");
+        userEvent.type(messageInput, "this test is so weird");
         
-    // const myName = screen.getByText(/Brad/i)
-    // expect (myName).toBeInTheDocument()
+        await act(async () =>{
+        const button = screen.getByRole("button",{name:/submit/i})
+         userEvent.click(button)
+        });
+        
+    const myName = screen.getByText(/Brad/i)
+    expect (myName).toBeInTheDocument()
 })
